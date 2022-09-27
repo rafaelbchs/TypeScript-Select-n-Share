8a9e34fbaeab23acfa5d6eb8163caeb9009395f5
rafaelbchs <rafaelbchacins@gmail.com>
2022-09-27 10:53:59 -0500

    started address handler

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,31 @@
+import axios from 'axios';
+
 // Code goes here!
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = 'AIzaSyCEWCzAtMRMhVE75vOkWCeUVnEI_R-z7hQ';
+
+type GoogleGeocodingResponse = {
+    results: {geometry: {location: {lat: number, lng: number}}}[];
+    status: 'OK' | 'ZERO_RESULTS';
+}
+
+function searchAddressHandler(event: Event){
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+    // send this to API
+    axios.get<GoogleGeocodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`).then(
+        response => {
+            if (response.data.status !== 'OK') {
+                throw new Error('Could not fetch location!')
+            }
+            const coordinates = response.data.results[0].geometry.location;
+        }
+    ).catch(err => 
+        alert(err.message)
+        console.log(err));
+
+}
+
+form?.addEventListener('submit', searchAddressHandler );
